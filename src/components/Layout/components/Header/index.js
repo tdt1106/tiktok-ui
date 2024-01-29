9b137fc0462f28d4a72ed512ed58e41cfab2d689
Tran Duc Tri <tranductri11062004@gmail.com>
2024-01-29 22:38:26 +0700

    98. Xây dựng UI phần Header #3

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
-import Tippy from '@tippyjs/react/headless'; 
+import { faCircleXmark, faMagnifyingGlass, faSignIn, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import Tippy from '@tippyjs/react/headless';
 
+import Button from '~/components/Button';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
@@ -12,54 +13,53 @@ import AccountItem from '~/components/AccountItem';
 const cx = classNames.bind(styles);
 
 function Header() {
-    const [searchResult, setSearchResult] = useState([])
+    const [searchResult, setSearchResult] = useState([]);
 
     useEffect(() => {
         setTimeout(() => {
             setSearchResult([]);
-        },0)
-    },[])
+        }, 0);
+    }, []);
 
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
                 {/* Logo */}
-                    <img src={images.logo} alt='Tiktok' />
+                <img src={images.logo} alt="Tiktok" />
                 {/* Search */}
                 <Tippy
-                        interactive
-                        visible={searchResult.length > 0}
-                        render={attrs => (
-                            <div className={cx('search-result')} tabIndex="-1" {...attrs}>
-                                    <PopperWrapper>
-                                        <h4 className={cx('search-title')}>
-                                            Accounts
-                                        </h4>
-                                        <AccountItem />
-                                        <AccountItem />
-                                        <AccountItem />
-                                        <AccountItem />
-                                    </PopperWrapper>
-                                </div>
-                        )}
-                    >
-                <div className={cx('search')}>
-                    <input placeholder='Search accounts and videos' spellCheck={false} />
-                    <button className={cx('clear')}>
-                    {/* Clear */}
-                        <FontAwesomeIcon icon={faCircleXmark}/>
-                    </button>
-                    {/* Loading */}
-                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner}/>
-                    
+                    interactive
+                    visible={searchResult.length > 0}
+                    render={(attrs) => (
+                        <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                            <PopperWrapper>
+                                <h4 className={cx('search-title')}>Accounts</h4>
+                                <AccountItem />
+                                <AccountItem />
+                                <AccountItem />
+                                <AccountItem />
+                            </PopperWrapper>
+                        </div>
+                    )}
+                >
+                    <div className={cx('search')}>
+                        <input placeholder="Search accounts and videos" spellCheck={false} />
+                        <button className={cx('clear')}>
+                            {/* Clear */}
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                        {/* Loading */}
+                        <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
                         <button className={cx('search-btn')}>
                             {/* Search */}
-                            <FontAwesomeIcon icon={faMagnifyingGlass}/>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
                         </button>
-                </div>
-                    </Tippy>
+                    </div>
+                </Tippy>
                 <div className={cx('actions')}>
-                    
+                    <Button text>Upload</Button>
+                    <Button primary >Log in</Button>
                 </div>
             </div>
         </header>
